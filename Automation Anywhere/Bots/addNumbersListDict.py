72d35a9d36390be813bc27a9291cbaff9d086f6a
Prashant Kumar prashant.kumar@automationanywhere.com <prashant.kumar@automationanywhere.com>
2020-10-08 17:55:11 +0000

    rr

---
+++ Automation Anywhere/Bots/addNumbersListDict.py
@@ -0,0 +1,12 @@
+def addList(listdata):
+    sum = 0
+    for i in listdata:
+        sum = sum + i
+    return sum
+
+def addDict(dict1):
+    sum=0
+    for values in dict1.values():
+        sum = sum + values
+    return sum
+
